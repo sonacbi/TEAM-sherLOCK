1e0c892471d1c42e735ad8725cd75fddad992370
hyeogi05 <jshkms5022@gmail.com>
2025-03-31 10:01:41 +0900

    fix: 가로 스크롤 mainpage 수정

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import '../styles/MainPage.css';
+import Scroll from '../components/Scroll/Scroll';
 
 function MainPage() {
   return (
     <div className='MainPage-wrap'>
       <h1>히히 하이요!!!!</h1>
+      <Scroll />
     </div>
   );
 }
