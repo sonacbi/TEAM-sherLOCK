fa74f7fb036b3016d11dfc5a6e7512a2542820a5
hyeogi05 <jshkms5022@gmail.com>
2025-04-16 19:57:47 +0900

    fix: Sign UI 수정, localhost:5173 들어가면 Intro 패스로 들어가지게 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import IntroPage from './pages/IntroPage';
 import MainPage from './pages/MainPage';
 import SocialAuthHandler from './pages/SocialAuthHandler'; // 카카오
@@ -8,9 +8,10 @@ function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Navigate to="/Intro" replace />} />
         <Route path="/Intro" element={<IntroPage />} />
         <Route path="/Main" element={<MainPage />} />
-        {/* 카카오/네이버 로그인용 path (추가) */}
+        {/* 카카오/네이버 로그인용 path */}
         <Route path="/auth/kakao" element={<SocialAuthHandler />} />
         <Route path="/auth/naver" element={<SocialAuthHandler />} />
       </Routes>
