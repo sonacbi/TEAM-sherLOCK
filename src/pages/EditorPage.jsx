5a2a52fac587b594c05c83dbbfaf5adccf8982cd
hyeogi05 <jshkms5022@gmail.com>
2025-05-22 17:48:53 +0900

    feat: 기본 도형 추가 완료

--- src/pages/EditorPage.jsx
+++ src/pages/EditorPage.jsx
@@ -13,10 +13,26 @@ import picture_icon from '../assets/images/EditorPage_img/picture_icon.png';
 import timer_icon from '../assets/images/EditorPage_img/timer_icon.png';
 import hint_icon from '../assets/images/EditorPage_img/hint_icon.png';
 import event_icon from '../assets/images/EditorPage_img/event_icon.png';
+import rectangle from '../assets/images/EditorPage_img/default_shape/rectangle.png';
+import circle from '../assets/images/EditorPage_img/default_shape/circle.png';
+import triangle from '../assets/images/EditorPage_img/default_shape/triangle.png';
+import rhombus from '../assets/images/EditorPage_img/default_shape/rhombus.png';
+import star from '../assets/images/EditorPage_img/default_shape/star.png';
+import heart from '../assets/images/EditorPage_img/default_shape/heart.png';
+import pentagon from '../assets/images/EditorPage_img/default_shape/pentagon.png';
+import trapezoid from '../assets/images/EditorPage_img/default_shape/trapezoid.png';
+import bubble1 from '../assets/images/EditorPage_img/bubble/bubble1.png';
+import bubble2 from '../assets/images/EditorPage_img/bubble/bubble2.png';
+import bubble3 from '../assets/images/EditorPage_img/bubble/bubble3.png';
+import bubble4 from '../assets/images/EditorPage_img/bubble/bubble4.png';
+import bubble5 from '../assets/images/EditorPage_img/bubble/bubble5.png';
+import bubble6 from '../assets/images/EditorPage_img/bubble/bubble6.png';
+import bubble7 from '../assets/images/EditorPage_img/bubble/bubble7.png';
+import bubble8 from '../assets/images/EditorPage_img/bubble/bubble8.png';
 
 function EditorPage() {
     const [addTextTrigger, setAddTextTrigger] = useState(0);
-    const [addShapeTrigger, setAddShapeTrigger] = useState(0);
+    const [addShapeTrigger, setAddShapeTrigger] = useState('');
     const [addImageFile, setAddImageFile] = useState(null);
     const [addFrameTrigger, setAddFrameTrigger] = useState(0);
 
@@ -37,10 +53,27 @@ function EditorPage() {
         setAddTextTrigger(Date.now());
         setSelectedTool('text');
     };
+    
+    const shapeImages = {
+        rectangle,
+        circle,
+        triangle,
+        rhombus,
+        star,
+        heart,
+        pentagon,
+        trapezoid,
+    };
 
-    const handleAddShape = () => {
-        setAddShapeTrigger(Date.now());
+    const handleShapeFineTuning = () => {
         setSelectedTool('shape');
+    };
+
+    const handleShapeClick = (shape) => {
+        setAddShapeTrigger(''); // 상태 초기화
+        setTimeout(() => {
+            setAddShapeTrigger(shape); // 선택한 도형 설정
+        }, 0);
     };
 
     const handleAddImage = () => {
@@ -108,7 +141,7 @@ function EditorPage() {
                             <p>텍스트</p>
                         </div>
 
-                        <div className={`shape_area ${selectedTool === 'shape' ? 'active' : ''}`} onClick={handleAddShape}>
+                        <div className={`shape_area ${selectedTool === 'shape' ? 'active' : ''}`} onClick={handleShapeFineTuning}>
                             <img id='shape_icon' src={shape_icon} alt='shape_icon' />
                             <p>도형</p>
                         </div>
@@ -163,7 +196,35 @@ function EditorPage() {
 
                         {selectedTool === 'shape' && (
                             <div className='shape_fine_tuning'>
-                                <h2>도형 세부조정</h2>
+                                <div className='default_shape'>
+                                    <label>기본 도형</label>
+                                    
+                                    <div className='default_shape_wrap'>
+                                        {Object.keys(shapeImages).map((shape) => (
+                                            <img
+                                                key={shape}
+                                                src={shapeImages[shape]}
+                                                alt={shape}
+                                                onClick={() => handleShapeClick(shape)}
+                                            />
+                                        ))}
+                                    </div>
+                                </div>
+
+                                <div className='bubble'>
+                                    <label>말풍선</label>
+                                    
+                                    <div className='bubble_wrap'>
+                                        <img id='bubble1' src={bubble1} alt='bubble1' />
+                                        <img id='bubble2' src={bubble2} alt='bubble2' />
+                                        <img id='bubble3' src={bubble3} alt='bubble3' />
+                                        <img id='bubble4' src={bubble4} alt='bubble4' />
+                                        <img id='bubble5' src={bubble5} alt='bubble5' />
+                                        <img id='bubble6' src={bubble6} alt='bubble6' />
+                                        <img id='bubble7' src={bubble7} alt='bubble7' />
+                                        <img id='bubble8' src={bubble8} alt='bubble8' />
+                                    </div>
+                                </div>
                             </div>
                         )}
 
