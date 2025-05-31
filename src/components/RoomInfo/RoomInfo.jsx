ddd98976c2c05a059b6e35e093fc85fbf3ca139e
hyeogi05 <jshkms5022@gmail.com>
2025-05-31 18:43:57 +0900

    feat: RoomInfo 컴포넌트 UI 90% 완성

--- src/components/RoomInfo/RoomInfo.jsx
+++ src/components/RoomInfo/RoomInfo.jsx
@@ -3,6 +3,12 @@ import React, { useState } from 'react';
 import './RoomInfo.css';
 
 import Room_thumbnail_basic_img from '../../assets/images/RoomInfo/Room_thumbnail_basic_img.png';
+import Room_theme_horror_img from '../../assets/images/MainPage_img/horror_icon.png'
+import Room_theme_adventure_img from '../../assets/images/MainPage_img/adventure_icon.png'
+import Room_theme_crime_img from '../../assets/images/MainPage_img/crime_icon.png'
+import public_icon from '../../assets/images/RoomInfo/public_icon.png';
+import private_icon from '../../assets/images/RoomInfo/private_icon.png';
+import limited_icon from '../../assets/images/RoomInfo/limited_icon.png';
 
 function RoomInfo() {
   const [thumbnail, setThumbnail] = useState(null);
@@ -20,7 +26,11 @@ function RoomInfo() {
       <div className='RoomInfo_content'>
         <div className='Room_title'>
           <p>제목</p>
-          <input type='text' placeholder='제목을 입력하세요.'></input>
+
+          <div className='text_precautions'>
+            <input type='text' placeholder='제목을 입력하세요.'></input>
+            <p></p>
+          </div>
         </div>
 
         <div className='Room_thumbnail'>
@@ -54,23 +64,99 @@ function RoomInfo() {
 
         <div className='Room_introduction'>
           <p>소개글</p>
-          <input type='text' placeholder='내용을 입력하세요.'></input>
+
+          <div className='introduction_precautions'>
+            <textarea placeholder='내용을 입력하세요.'>
+
+            </textarea>
+
+            <p></p>
+          </div>
         </div>
 
         <div className='Room_difficulty'>
-          <p>난이도</p>
+          <div className='Room_difficulty_select'>
+            <p>난이도</p>
+            <span>선택: ??</span>
+          </div>
+
+          <div className='difficulty_wrap'>
+            <div className='difficulty_one'><p>1</p></div>
+            <div className='difficulty_two'><p>2</p></div>
+            <div className='difficulty_three'><p>3</p></div>
+            <div className='difficulty_four'><p>4</p></div>
+            <div className='difficulty_five'><p>5</p></div>
+          </div>
         </div>
 
         <div className='Room_playtime'>
           <p>예상소요시간</p>
+
+          <div className='playtime_wrap'>
+            <div className='hour_wrap'>
+              <input type='text' placeholder='?'></input>
+              <p>h</p>
+            </div>
+
+            <div className='min_wrap'>
+              <input type='text' placeholder='??'></input>
+              <p>m</p>
+            </div>
+          </div>
         </div>
 
         <div className='Room_theme'>
-          <p>테마</p>
+          <div className='Room_theme_select'>
+            <p>테마</p>
+            <span>선택: ??</span>
+          </div>
+
+          <div className='theme_wrap'>
+            <div className='theme_horror'>
+              <img id='theme_horror_img' src={Room_theme_horror_img} alt='theme_horror_img' />
+            </div>
+
+            <div className='theme_adventure'>
+              <img id='theme_adventure_img' src={Room_theme_adventure_img} alt='theme_adventure_img' />
+            </div>
+
+            <div className='theme_crime'>
+              <img id='theme_crime_img' src={Room_theme_crime_img} alt='theme_crime_img' />
+            </div>
+          </div>
         </div>
 
         <div className='Room_visibility'>
           <p>공개여부</p>
+
+          <div className='visibility_wrap'>
+            <div className='public'>
+              <div className='icon_wrap'>
+                <img id='public_icon' src={public_icon} alt='public_icon' />
+              </div>
+              
+              <input type='radio' name='visibility' value='public'></input>
+              <p>공개</p>
+            </div>
+
+            <div className='limited'>
+              <div className='icon_wrap'>
+                <img id='limited_icon' src={limited_icon} alt='limited_icon' />
+              </div>
+
+              <input type='radio' name='visibility' value='limited'></input>
+              <p>일부공개</p>
+            </div>
+
+            <div className='private'>
+              <div className='icon_wrap'>
+                <img id='private_icon' src={private_icon} alt='private_icon' />
+              </div>
+
+              <input type='radio' name='visibility' value='private'></input>
+              <p>비공개</p>
+            </div>
+          </div>
         </div>
 
         <div className='Room_creator'>
