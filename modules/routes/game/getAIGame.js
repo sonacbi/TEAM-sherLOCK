9c106bfc2d82ee54d5bfa867292367749c61b8a4
gourn0257 <nes.jbch@gmail.com>
2025-10-29 09:39:27 +0900

    발표용

--- modules/routes/game/getAIGame.js
+++ modules/routes/game/getAIGame.js
@@ -3,17 +3,21 @@ import fs from 'fs';
 
 const router = express.Router();
 
-const fileUrl = 'C:/Users/user11/Downloads/Stage2v3.zip'
+const fileUrl1 = 'C:/Users/user11/Downloads/SRP.zip'
+const fileUrl2 = 'C:/Users/user11/Downloads/park.zip'
+let num = 0;
 
 router.get("/", async (req, res) => {
   console.log('get/ai_game query', req.query);
-  if (!fs.existsSync(fileUrl)) {
+  if (!fs.existsSync(fileUrl1) || !fs.existsSync(fileUrl2)) {
     return res.status(404).send('파일이 존재하지 않습니다.');
   }
   try {
+    const fileUrl = num % 2 === 0 ? fileUrl1 : fileUrl2;
     const readStream = fs.createReadStream(fileUrl);
     res.setHeader('Content-Type', 'application/zip');
     readStream.pipe(res);
+    num++;
     console.log('잘 보냈음');
   } catch (err) {
     console.log('실패')
